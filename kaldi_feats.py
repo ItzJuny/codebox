2c4262a0368ddf182f76867050a05cfdc9d77c7b
junono97 <57182325+junono97@users.noreply.github.com>
2021-12-03 22:14:20 +0800

    Create kaldi_feats.py
    用KALDI来生成MFCC和CMVN特征文件。

---
+++ kaldi_feats.py
@@ -0,0 +1,26 @@
+import os
+import numpy as np 
+path="/home/jun/Class/temp/wav"
+savedir="/home/jun/Class/temp/wavfeat"
+# loading wavfiles
+wavfiles=[os.path.join(path,i) for i in os.listdir(path) if os.path.splitext(i)[-1]==".wav"]
+if not os.path.exists(savedir):
+    os.makedirs(savedir,exist_ok=True)
+
+# wav.scp
+f=open(os.path.join(savedir,"wav.scp"),"w")
+for i in wavfiles:
+    f.write("%s %s\n"%(os.path.basename(i),i))
+f.close()
+
+# feats.ark(MFCC)
+opt="compute-mfcc-feats --use-energy=false scp:%s/wav.scp ark:%s/feats.ark"%(savedir,savedir)
+os.system(opt)
+
+# feat.scp(MFCC) feat.ark(MFCC)
+opt="copy-feats ark:%s/feats.ark ark,scp:%s/feat.ark,%s/feat.scp"%(savedir,savedir,savedir)
+os.system(opt)
+
+# cmvn.scp cmvn.ark(CMVN)
+opt="compute-cmvn-stats scp:%s/feat.scp ark,scp:%s/cmvn.ark,%s/cmvn.scp"%(savedir,savedir,savedir)
+os.system(opt)
